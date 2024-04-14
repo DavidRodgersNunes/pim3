372e7f7c0e8bff1e1e74aa9a38bd11b6e0623ca2
DavidRodgersNunes <davidrodgers.inv@gmail.com>
2024-04-14 12:21:24 -0300

    Classe Validação

---
+++ SoftwarePIM/SoftwarePIM/modelo/Validacao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SoftwarePIM.modelo
+{
+    public class Validacao
+    {
+        public int idadei;
+        public string tamanhonome;
+        public string tamanhoidade;
+        public string mensagem;
+
+        public void Validar(string nome, string idade)
+        {
+            mensagem = "";
+            try 
+            { 
+                idadei=Convert.ToInt32(idade);
+                string pattern = @"[^\w]"; // Identifica qualquer caractere que não seja alfanumérico
+
+                if (Regex.IsMatch(nome, pattern))
+                {
+                    throw new Exception("O nome não pode conter caracteres especiais.");
+                }
+
+            }
+            catch (Exception)
+            {               
+                mensagem = "Digite números válidos";
+            }
+        }
+    }
+}
